ed2daa096ac469ff161bfb5ef3307d772ff5fa65
lppt <tvorimir@gmail.com>
2024-12-07 19:21:44 +0300

    Перевод на PKCE + добавил api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,22 @@ import ReportPage from './components/ReportPage';
 const keycloakConfig: KeycloakConfig = {
   url: process.env.REACT_APP_KEYCLOAK_URL,
   realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || ""
 };
+
 
 const keycloak = new Keycloak(keycloakConfig);
 
+
+const keycloakOptions={
+  onLoad: "check-sso",
+  pkceMethod: "S256",
+  silentCheckSsoRedirectUri:
+  window.location.origin + "/silent-check-sso.html"
+};
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={keycloakOptions} >
       <div className="App">
         <ReportPage />
       </div>
