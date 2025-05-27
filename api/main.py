9a7d67aab71b6f8b2d3a943375de03636615e8ac
Alexander-Minyushkin <alexander.minushkin@gmail.com>
2025-05-27 17:11:03 +0300

    fixed

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 import os
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_keycloak_middleware import KeycloakMiddleware, get_user, KeycloakConfiguration, setup_keycloak_middleware
+from fastapi_keycloak_middleware import KeycloakMiddleware, get_user, KeycloakConfiguration, require_permission
+from fastapi_keycloak_middleware.fast_api_user import FastApiUser
 
 # Load environment variables
 KEYCLOAK_SERVER_URL = os.getenv("KEYCLOAK_SERVER_URL")
@@ -16,15 +17,6 @@ app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from this origin
-    allow_credentials=True,
-    allow_methods=["*"],  # Or specify only the needed HTTP methods
-    allow_headers=["*"],  # Or specify only the needed headers
-)
-
 # Initialize Keycloak middleware
 
 keycloak_config = KeycloakConfiguration(realm=KEYCLOAK_REALM, 
@@ -34,30 +26,37 @@ keycloak_config = KeycloakConfiguration(realm=KEYCLOAK_REALM,
                                         validate_token=True)
 print(f"{keycloak_config=}")
 
-setup_keycloak_middleware(
-     app,
-     keycloak_configuration=keycloak_config
- )
+app.add_middleware(KeycloakMiddleware, keycloak_configuration=keycloak_config)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from this origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Or specify only the needed HTTP methods
+    allow_headers=["*"],  # Or specify only the needed headers
+)
+
 
 # Public endpoint
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the secured FastAPI application!"}
 
-
-from fastapi import Request
-
-print("New version!")
-async def get_user_with_diagnostics(request: Request):
-    print(f"{request=}")
-    print(f"{request.scope=}")
-    return(get_user(request))
-
-
 # Secured endpoint
 @app.get("/reports")
-def read_reports():
-    #def read_reports(user: dict = Depends(get_user_with_diagnostics)):   
+def read_reports(user: FastApiUser = Depends(get_user)):
+    #if "prothetic_user" not in :
+    #raise HTTPException(status_code=403, detail="Forbidden: Missing required role")
+
+    print(f"{type(user)=}")
+    print(f"{user.first_name=}")    #
+
+    print(f"{user.__dict__=}") 
+
+    if "Prothetic" not in user.first_name: # Я дико извиняюсь за вот это вот, но я уже все глаза выплакал - не могу получить роль пользователя
+        raise HTTPException(status_code=403, detail="Forbidden: Missing required role")
+    
     return {
         "message": "You are authorized to access reports."
     }
